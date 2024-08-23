03b19618e1876bf71ed18ab770fca649a74b3f66
nishukum1211 <nishukum1211@gmail.com>
2024-08-23 13:59:02 +0530

    initial commit

--- src/components/Head.js
+++ src/components/Head.js
@@ -12,20 +12,12 @@ const Head = () => {
   const searchCache = useSelector((store) => store.search);
   const dispatch = useDispatch();
 
-
-  /**
-   *  searchCache = {
-   *     "iphone": ["iphone 11", "iphone 14"]
-   *  }
-   *  searchQuery = iphone
-   */
-
   useEffect(() => {
     const timer = setTimeout(() => {
       if (searchCache[searchQuery]) {
         setSuggestions(searchCache[searchQuery]);
       } else {
-        getSearchSugsestions();
+        getSearchSuggestions();
       }
     }, 200);
 
@@ -34,10 +26,9 @@ const Head = () => {
     };
   }, [searchQuery]);
 
-  const getSearchSugsestions = async () => {
+  const getSearchSuggestions = async () => {
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
-    //console.log(json[1]);
     setSuggestions(json[1]);
 
     dispatch(
@@ -45,9 +36,7 @@ const Head = () => {
         [searchQuery]: json[1],
       })
     );
-
   };
-  
 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
@@ -55,40 +44,43 @@ const Head = () => {
 
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
-      <div className="flex col-span-1">
+      <div className="flex items-center col-span-1">
         <img
           onClick={() => toggleMenuHandler()}
-          className="h-8 cursor-pointer"
+          className="h-6 cursor-pointer lg:h-8"
           alt="menu"
           src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="
         />
         <a href="/">
           <img
-            className="h-8 mx-2"
+            className="h-6 ml-2 lg:h-8 lg:ml-4"
             alt="youtube-logo"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
           />
         </a>
       </div>
-      <div className="col-span-10 px-10">
-        <div>
+      <div className="col-span-8 flex justify-center items-center lg:col-span-10 px-2 lg:px-10">
+        <div className="flex w-full lg:w-1/2">
           <input
-            className="px-5 w-1/2 border border-gray-400 py-2 rounded-l-full h-5"
+            className="px-2 py-1 lg:px-5 lg:py-2 border border-gray-400 rounded-l-full w-full"
             type="text"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             onFocus={() => setShowSuggestions(true)}
             onBlur={() => setShowSuggestions(false)}
           />
-          <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100 h-9">
+          <button className="border border-gray-400 px-2 lg:px-5 py-1 lg:py-2 rounded-r-full bg-gray-100">
             🔍
           </button>
         </div>
         {showSuggestions && (
-          <div className="fixed bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
+          <div className="fixed bg-white py-2 px-2 w-[90%] sm:w-[37rem] shadow-lg rounded-lg border border-gray-100 mt-2">
             <ul>
               {suggestions.map((s) => (
-                <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+                <li
+                  key={s}
+                  className="py-2 px-3 shadow-sm hover:bg-gray-100 cursor-pointer"
+                >
                   🔍 {s}
                 </li>
               ))}
@@ -96,9 +88,9 @@ const Head = () => {
           </div>
         )}
       </div>
-      <div className="col-span-1">
+      <div className="col-span-1 flex justify-end items-center">
         <img
-          className="h-8"
+          className="h-6 lg:h-8"
           alt="user"
           src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
         />
